62f6fd6b4273af9b005293ec2f44274c6208161d
Andres <andy3blender@gmail.com>
2021-12-22 16:42:20 -0500

    redirect login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   //   children: homeRoutes
   // },
   { path:'login', component: LoginComponent },
-    
+  { path: '**', redirectTo: 'login' },
   {
     path: '',
     canLoad: [AuthGuard],
