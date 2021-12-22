d7ca3218d52f43da722dd9b75e62ff33fdbc2a7e
andy3blender <73072602+andy3blender@users.noreply.github.com>
2021-12-22 16:38:43 -0500

    add https api

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,2 +1,2 @@
-export const apiUrl = 'http://www.sirilapp.somee.com/api' //'https://localhost:44374/api';
-export const serverUrl = 'http://www.sirilapp.somee.com' //'https://localhost:44374/';
+export const apiUrl = 'https://www.sirilapp.somee.com/api' //'https://localhost:44374/api';
+export const serverUrl = 'https://www.sirilapp.somee.com' //'https://localhost:44374/';
